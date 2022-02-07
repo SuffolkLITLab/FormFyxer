ba1d31bda7dca60c51d3ac4c2c08a0b9b606f005
David Colarusso <dcolarusso@adm.suffolk.edu>
2022-02-07 13:04:53 -0500

    first commit

---
+++ lit_explorer.py
@@ -0,0 +1,474 @@
+# Updated on 2021-11-30
+
+import os
+import re
+import spacy
+import PyPDF2
+import pikepdf
+import textstat
+import requests
+import json
+import numpy as np
+from numpy import unique
+from numpy import where
+from sklearn.cluster import AffinityPropagation
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.preprocessing import normalize
+from joblib import load
+from nltk.corpus import stopwords
+import math
+import signal
+from contextlib import contextmanager
+
+stop_words = set(stopwords.words('english'))
+nlp = spacy.load('en_core_web_lg') # this takes a while to loadimport os
+
+
+# Load local variables, models, and API key(s).
+
+included_fields = load(os.path.join(os.path.dirname(__file__), 'data', 'included_fields.joblib'))
+jurisdictions = load(os.path.join(os.path.dirname(__file__), 'data', 'jurisdictions.joblib'))
+groups = load(os.path.join(os.path.dirname(__file__), 'data', 'groups.joblib'))
+clf_field_names = load(os.path.join(os.path.dirname(__file__), 'data', 'clf_field_names.joblib'))
+with open(os.path.join(os.path.dirname(__file__), '../../data/keys', 'spot_token.txt'), 'r') as file:
+    spot_token = file.read().rstrip()
+
+
+# This creates a timeout exception that can be triggered when something hangs too long. 
+
+class TimeoutException(Exception): pass
+@contextmanager
+def time_limit(seconds):
+    def signal_handler(signum, frame):
+        raise TimeoutException("Timed out!")
+    signal.signal(signal.SIGALRM, signal_handler)
+    signal.alarm(seconds)
+    try:
+        yield
+    finally:
+        signal.alarm(0)
+
+# Pull ID values out of the LIST/NSMI results from Spot.
+
+def recursive_get_id(values_to_unpack, tmpl=None):
+    # h/t to Quinten and Bryce for this code ;)
+    if not tmpl:
+        tmpl = set()
+    if isinstance(values_to_unpack, dict):
+        tmpl.add(values_to_unpack.get('id'))
+        if values_to_unpack.get('children'):
+            tmpl.update(recursive_get_id(values_to_unpack.get('children'), tmpl))
+        return tmpl
+    elif isinstance(values_to_unpack, list):
+        for item in values_to_unpack:
+            tmpl.update(recursive_get_id(item, tmpl))
+        return tmpl
+    else:
+        return set()
+
+# Call the Spot API, but return only the IDs of issues found in the text.
+
+def spot(text,lower=0.25,pred=0.5,upper=0.6):
+
+    headers = { "Authorization": "Bearer " + spot_token, "Content-Type":"application/json" }
+
+    body = {
+      "text": text,
+      "save-text": 0,
+      "cutoff-lower": lower,
+      "cutoff-pred": pred,
+      "cutoff-upper": upper
+    }
+
+    r = requests.post('https://spot.suffolklitlab.org/v0/entities-nested/', headers=headers, data=json.dumps(body))
+    output_ = r.json()
+
+    try:
+        return list(recursive_get_id(output_["labels"]))
+    except:
+        return []
+
+# A function to pull words out of snake_case, camelCase and the like.
+
+def reCase(text):
+    output = re.sub("(\w|\d)(_|-)(\w|\d)","\\1 \\3",text.strip())
+    output = re.sub("([a-z])([A-Z]|\d)","\\1 \\2",output)
+    output = re.sub("(\d)([A-Z]|[a-z])","\\1 \\2",output)
+    output = re.sub("([A-Z]|[a-z])(\d)","\\1 \\2",output)
+    return output
+
+# Takes text from an auto-generated field name and uses regex to convert it into an Assembly Line standard field.
+# See https://suffolklitlab.org/docassemble-AssemblyLine-documentation/docs/label_variables/
+
+def regex_norm_field(text):
+    
+    regex_list = [
+
+        # Personal info
+        ## Name & Bio
+        ["^((My|Your|Full( legal)?) )?Name$","users1_name"],
+        ["^(Typed or )?Printed Name\s?\d*$","users1_name"],
+        ["^(DOB|Date of Birth|Birthday)$","users1_birthdate"],
+        ## Address
+        ["^(Street )?Address$","users1_address_line_one"],
+        ["^City State Zip$","users1_address_line_two"],
+        ["^City$","users1_address_city"],
+        ["^State$","users1_address_state"],
+        ["^Zip( Code)?$","users1_address_zip"],
+        ## Contact
+        ["^(Phone|Telephone)$","users1_phone_number"],
+        ["^Email( Adress)$","users1_email"],
+
+        # Parties
+        ["^plaintiff\(?s?\)?$","plantiff1_name"],
+        ["^defendant\(?s?\)?$","defendant1_name"],
+        ["^petitioner\(?s?\)?$","petitioners1_name"],
+        ["^respondent\(?s?\)?$","respondents1_name"],
+
+        # Court info
+        ["^(Court\s)?Case\s?(No|Number)?\s?A?$","docket_number"],
+        ["^File\s?(No|Number)?\s?A?$","docket_number"],
+
+        # Form info
+        ["^(Signature|Sign( here)?)\s?\d*$","users1_signature"],
+        ["^Date\s?\d*$","signature_date"],
+    ]
+
+    for regex in regex_list:
+        text = re.sub(regex[0],regex[1],text, flags=re.IGNORECASE)
+    return text
+
+# Tranforms a string of text into a snake_case variable close in length to `max_length` name by summarizing the string and stiching the summary together in snake_case. h/t h/t https://towardsdatascience.com/nlp-building-a-summariser-68e0c19e3a93
+
+def reformat_field(text,max_length=30):
+    orig_title = text.lower()
+    orig_title = re.sub("[^a-zA-Z]+"," ",orig_title)
+    orig_title_words = orig_title.split()
+
+    deduped_sentence = []
+    for word in orig_title_words:
+        if word not in deduped_sentence:
+            deduped_sentence.append(word)
+
+    filtered_sentence = [w for w in deduped_sentence if not w.lower() in stop_words]
+
+    filtered_title_words = filtered_sentence
+
+    characters = len(' '.join(filtered_title_words))
+
+    if characters > 0:
+
+        words = len(filtered_title_words)
+        av_word_len = math.ceil(len(' '.join(filtered_title_words))/len(filtered_title_words))
+        x_words = math.floor((max_length)/av_word_len)
+
+
+        sim_mat = np.zeros([len(filtered_title_words),len(filtered_title_words)])
+        # for each word compared to other
+        for i in range(len(filtered_title_words)):
+            for j in range(len(filtered_title_words)):
+                if i != j:
+                    sim_mat[i][j] = cosine_similarity(nlp(filtered_title_words[i]).vector.reshape(1,300), nlp(filtered_title_words[j]).vector.reshape(1,300))[0,0]
+
+        try:
+            nx_graph = nx.from_numpy_array(sim_mat)
+            scores = nx.pagerank(nx_graph)
+            sorted_scores = sorted(scores.items(), key=lambda item: item[1], reverse=True)
+
+            if x_words > len(scores):
+                x_words=len(scores)
+
+            i = 0
+            new_title = ""
+            for x in filtered_title_words:
+                if scores[i] >= sorted_scores[x_words-1][1]:
+                    if len(new_title)>0: new_title+="_"
+                    new_title += x
+                i+=1
+
+            return new_title
+        except:
+            return '_'.join(filtered_title_words)
+    else:
+        if re.search("^(\d+)$", text):
+            return "unknown"
+        else:
+            return re.sub("\s+","_",text.lower())
+
+# Vectorize a string of text. 
+
+def vectorize(text):
+    output = nlp(str(text)).vector
+    return output
+
+# Normalize a word vector.
+
+def norm(row):
+    try:
+        matrix = row.reshape(1,-1).astype(np.float64)
+        return normalize(matrix, axis=1, norm='l1')[0]
+    except Exception as e:
+        print("===================")
+        print("Error: ",e)
+        print("===================")
+        return np.NaN
+
+# Given an auto-generated field name and context from the form where it appeared, this function attempts to normalize the field name. Here's what's going on:
+# 1. It will `reCase` the variable text
+# 2. Then it will run the output through `regex_norm_field`
+# 3. If it doesn't find anything, it will use the ML model `clf_field_names`
+# 4. If the prediction isn't very confident, it will run it through `reformat_field`  
+
+def normalize_name(jur,group,n,per,last_field,this_field):
+
+    # Add hard coded conversions maybe by calling a function
+    # if returns 0 then fail over to ML or otherway around poor prob -> check hard-coded
+
+    if this_field not in included_fields:
+        this_field = reCase(this_field)
+
+        out_put = regex_norm_field(this_field)
+        conf = 1.0
+
+        if out_put==this_field:
+            params = []
+            for item in jurisdictions:
+                if jur== item:
+                    params.append(1)
+                else:
+                    params.append(0)
+            for item in groups:
+                if group== item:
+                    params.append(1)
+                else:
+                    params.append(0)
+            params.append(n)
+            params.append(per)
+            for vec in norm(vectorize(this_field)):
+                params.append(vec)
+
+            for item in included_fields:
+                if last_field==item:
+                    params.append(1)
+                else:
+                    params.append(0)
+
+            pred = clf_field_names.predict([params])
+            prob = clf_field_names.predict_proba([params])
+
+            conf = prob[0].tolist()[prob[0].tolist().index(max(prob[0].tolist()))]
+            out_put = pred[0]
+
+    else:
+        out_put = this_field
+        conf = 1
+
+    if out_put in included_fields:
+        if conf >= 0:
+            return "*"+out_put,conf # this * is a hack to show when something is in the list of known fields later. I need to fix this
+        else:
+            return reformat_field(this_field),conf
+    else:
+        return reformat_field(this_field),conf
+
+# Take a list of AL variables and spits out suggested groupings. Here's what's going on:
+# 
+# 1. It reads in a list of fields (e.g., `["user_name","user_address"]`)
+# 2. Splits each field into words (e.g., turning `user_name` into `user name`)
+# 3. It then turns these ngrams/"sentences" into vectors using word2vec. 
+# 4. For the collection of fields, it finds clusters of these "sentences" within the semantic space defined by word2vec. Currently it uses Affinity Propagation. See https://machinelearningmastery.com/clustering-algorithms-with-python/
+
+def cluster_screens(fields=[],damping=0.7):
+    # Takes in a list (fields) and returns a suggested screen grouping
+    # Set damping to value >= 0.5 or < 1 to tune how related screens should be
+
+    vec_mat = np.zeros([len(fields),300])
+    for i in range(len(fields)):
+        vec_mat[i] = [nlp(reCase(fields[i])).vector][0]
+
+    # create model
+    model = AffinityPropagation(damping=damping,random_state=4)
+    # fit the model
+    model.fit(vec_mat)
+    # assign a cluster to each example
+    yhat = model.predict(vec_mat)
+    # retrieve unique clusters
+    clusters = unique(yhat)
+
+    screens = {}
+    #sim = np.zeros([5,300])
+    i=0
+    for cluster in clusters:
+        this_screen = where(yhat == cluster)[0]
+        vars = []
+        j=0
+        for screen in this_screen:
+            #sim[screen]=vec_mat[screen] # use this spot to add up vectors for compare to list
+            vars.append(fields[screen])
+            j+=1
+        screens["screen_%s"%i]=vars
+        i+=1
+
+    return screens
+
+# Get the text content of a pdf.
+
+def read_pdf (file):
+    try:
+        pdfFile = PyPDF2.PdfFileReader(open(file, "rb"))
+        if pdfFile.isEncrypted:
+            try:
+                pdfFile.decrypt('')
+                #print ('File Decrypted (PyPDF2)')
+            except:
+                #
+                #
+                # This didn't go so well on my Windows box so I just ran this in the pdf folder's cmd:
+                # for %f in (*.*) do copy %f temp.pdf /Y && "C:\Program Files (x86)\qpdf-8.0.2\bin\qpdf.exe" --password="" --decrypt temp.pdf %f
+                #
+                #
+                #
+                
+                command="cp "+file+" tmp/temp.pdf; qpdf --password='' --decrypt tmp/temp.pdf "+file
+                os.system(command)
+                #print ('File Decrypted (qpdf)')
+                #re-open the decrypted file
+                pdfFile = PyPDF2.PdfFileReader(open(file, "rb"))
+        text = ""
+        for page in pdfFile.pages:
+            text = text + " " + page.extractText()
+        text = reCase(text)
+        text = re.sub("(\.|,|;|:|!|\?|\n|\]|\))","\\1 ",text)
+        text = re.sub("(\(|\[)"," \\1",text)
+        text = re.sub(" +"," ",text)
+        return text
+    except:
+        return ""
+
+# Read in a pdf, pull out basic stats, attempt to normalize its form fields, and re-write the file with the new fields (if `rewrite=1`). 
+
+def parse_form(fileloc,title=None,jur=None,cat=None,normalize=1,use_spot=0,rewrite=0):
+    f = PyPDF2.PdfFileReader(fileloc)
+
+    if f.isEncrypted:
+        pdf = pikepdf.open(fileloc, allow_overwriting_input=True)
+        pdf.save(fileloc)
+        f = PyPDF2.PdfFileReader(fileloc)
+        
+    npages = f.getNumPages()
+  
+    # When reading some pdfs, this can hang due to their crazy field structure
+    try:
+        with time_limit(15):
+            ff = f.getFields()
+    except TimeoutException as e:
+        print("Timed out!")
+        ff = None   
+    
+    if ff:
+        fields = list(ff.keys())
+    else:
+        fields = []
+    f_per_page = len(fields)/npages
+    text = read_pdf(fileloc)
+    
+    if title is None:
+        title = reCase(re.search("(.*)\n",text).group(1).strip())
+
+    try:
+        #readbility = int(Readability(text).flesch_kincaid().grade_level)
+        text = re.sub("_"," ",text)
+        text = re.sub("\n",". ",text)
+        text = re.sub(" +"," ",text)
+        if text!= "":
+            consensus = textstat.text_standard(text)
+            readbility = eval(re.sub("^(\d+)[^0-9]+(\d+)\w*.*","(\\1+\\2)/2",consensus))
+        else:
+            readbility = None
+    except:
+        readbility = None
+
+    if use_spot==1:
+        nmsi = spot(title + ". " +text)      
+    else:
+        nmsi = []
+        
+    if normalize==1:
+        i = 0 
+        length = len(fields)
+        last = "null"
+        new_fields = []
+        new_fields_conf = []
+        for field in fields:
+            #print(jur,cat,i,i/length,last,field)
+            this_field,this_conf = normalize_name(jur,cat,i,i/length,last,field)
+            new_fields.append(this_field)
+            new_fields_conf.append(this_conf)
+            last = field
+        
+        new_fields = [v + "__" + str(new_fields[:i].count(v) + 1) if new_fields.count(v) > 1 else v for i, v in enumerate(new_fields)]
+    else:
+        new_fields = fields
+        new_fields_conf = []
+    
+    stats = {
+            "title":title,
+            "category":cat,
+            "pages":npages,
+            "reading grade level": readbility,
+            "list":nmsi,
+            "avg fields per page": f_per_page,
+            "fields":new_fields,
+            "fields_conf":new_fields_conf,
+            "fields_old":fields,
+            "text":text
+            }    
+    
+    if rewrite==1:
+        try:
+            if 1==1:
+                my_pdf = pikepdf.Pdf.open(fileloc, allow_overwriting_input=True)
+                fields_too = my_pdf.Root.AcroForm.Fields #[0]["/Kids"][0]["/Kids"][0]["/Kids"][0]["/Kids"]
+                #print(repr(fields_too))
+                
+                k =0
+                for field in new_fields:
+                    #print(k,field)
+                    fields_too[k].T = re.sub("^\*","",field)
+                    k+=1
+
+                #f2.T = 'new_hospital_name'
+                #filename = re.search("\/(\w*\.pdf)$",fileloc).groups()[0]
+                #my_pdf.save('/%s'%(filename))
+                my_pdf.save(fileloc)
+            else:
+                file = PdfFileWriter()
+
+                first_page = f.getPage(0)
+
+                file.cloneDocumentFromReader(f)
+                #file.appendPagesFromReader(f)
+
+                x ={}
+                for y in ff:
+                    x[y]=""
+
+                #print(x)
+
+                file.updatePageFormFieldValues(first_page,x)
+
+                output = open('blankPdf.pdf', 'wb')
+                file.write(output)  
+        except:
+            error = "could not change form fields"
+    
+    return stats
+
+def form_complexity(text,fields,reading_lv):
+    
+    # check for fields that requier user to look up info, when found add to complexity
+    # maybe score these by minutes to recall/fill out
+    # so, figure out words per minute, mix in with readability and page number and field numbers
+    
+    return 0
+
---
+++ pdf_wrangling.py
@@ -0,0 +1,249 @@
+import io
+from enum import Enum
+import tempfile
+from typing import Any, Dict, Iterable, Union
+
+import cv2
+import numpy as np
+from pdf2image import convert_from_path
+from pikepdf import Pdf
+from reportlab.pdfgen import canvas
+from reportlab.lib.colors import magenta, pink, blue 
+
+######## PDF internals related funcitons ##########
+
+class FieldType(Enum):
+    TEXT = 'text' # Text input Field
+    CHECK_BOX = 'checkbox'
+    LIST_BOX = 'listbox' # allows multiple selection
+    CHOICE = 'choice' # allows only one selection
+    RADIO = 'radio'
+
+class FormField:
+    """A data holding class, used to easily specify how a PDF form field should be created."""
+    def __init__(self, program_name:str, type_name:Union[FieldType, str], x:int, y:int, 
+                 user_name:str='', configs:Dict[str, Any]=None):
+        """
+        Constructor
+        
+        Args:
+            x: the x position of the lower left corner of the field. Should be in X,Y coordinates,
+                where (0, 0) is the lower left of the page, x goes to the right, and units are in
+                points (1/72th of an inch)
+            y: the y position of the lower left corner of the field. Should be in X,Y coordinates,
+                where (0, 0) is the lower left of the page, y goes up, and units are in points 
+                (1/72th of an inch)
+            config: a dictionary containing any keyword argument to the reportlab field functions,
+                which will vary depending on what type of field this is. See section 4.7 of the 
+                [reportlab User Guide](https://www.reportlab.com/docs/reportlab-userguide.pdf)
+        
+        """
+        if isinstance(type_name, str):
+            self.type = FieldType(type_name.lower()) # throws a ValueError, keeping in for now
+        else:
+            self.type = type_name
+        self.name = program_name
+        self.x = x
+        self.y = y
+        self.user_name = user_name
+        # TODO(brycew): If we aren't given options, make our own depending on self.type
+        if self.type == FieldType.CHECK_BOX:
+            self.configs= {
+                'buttonStyle': 'check',
+                'borderColor': magenta,
+                'fillColor' :pink,
+                'textColor':blue,
+                'forceBorder':True
+            }
+        elif self.type == FieldType.TEXT:
+            self.configs = {
+                'fieldFlags': 'doNotScroll'
+            }
+        else:
+            self.configs = {}
+        if configs:
+            self.configs.update(configs)
+    def __str__(self):
+        return f'Type: {self.type}, Name: {self.name}, User name: {self.user_name}, X: {self.x}, Y: {self.y}, Configs: {self.configs}'
+
+    def __repr__(self):
+        return str(self)
+
+def _create_only_fields(io_obj, fields_per_page:Iterable[Iterable[FormField]], font_name:str='Courier', font_size:int=20):
+    """Creates a PDF that contains only AcroForm fields. This PDF is then merged into an existing PDF to add fields to it.
+    We're adding fields to a PDF this way because reportlab isn't able to read PDFs, but is the best feature library for
+    writing them.
+    """
+    c = canvas.Canvas(io_obj)
+    c.setFont(font_name, font_size)
+    form = c.acroForm
+    for fields in fields_per_page:
+        for field in fields:
+            if field.type == FieldType.TEXT: 
+                form.textfield(name=field.name, tooltip=field.user_name, 
+                              x=field.x, y=field.y, **field.configs)
+            elif field.type == FieldType.CHECK_BOX: 
+                form.checkbox(name=field.name, tooltip=field.user_name,
+                              x=field.x, y=field.y, **field.configs) 
+            elif field.type == FieldType.LIST_BOX: 
+                form.listbox(name=field.name, tooltip=field.user_name,
+                              x=field.x, y=field.y, **field.configs)
+            elif field.type == FieldType.CHOICE: 
+                form.choice(name=field.name, tooltip=field.user_name,
+                            x=field.x, y=field.y, **field.configs)
+            elif field.type == FieldType.RADIO: 
+                form.radio(name=field.name, tooltip=field.user_name,
+                            x=field.x, y=field.y, **field.configs)
+            else:
+                pass
+        c.showPage() # Goes to the next page
+    c.save()
+
+def set_fields(in_file, out_file, fields_per_page:Iterable[Iterable[FormField]]):
+    """Adds fields per page to the in_file PDF, writing the new PDF to out_file.
+
+    Example usage:
+    ```
+    set_fields('no_fields.pdf', 'single_field_on_second_page.pdf', 
+      [
+        [],  # nothing on the first page
+        [ # Second page
+          FormField('new_field', 'text', 110, 105, configs={'width': 200, 'height': 30}),
+          # Choice needs value to be one of the possible options, and options to be a list of strings or tuples
+          FormField('new_choices', 'choice', 110, 400, configs={'value': 'Option 1', 'options': ['Option 1', 'Option 2']}),
+          # Radios need to have the same name, with different values
+          FormField('new_radio1', 'radio', 110, 600, configs={'value': 'option a'}),
+          FormField('new_radio1', 'radio', 110, 500, configs={'value': 'option b'})
+        ] 
+      ]
+    )
+    """
+    if not fields_per_page:
+        # Nothing to do, lol
+        return
+    in_pdf = Pdf.open(in_file)
+    if hasattr(in_pdf.Root, 'AcroForm'):
+        print('Not going to overwrite the existing AcroForm!')
+        return None
+    # Make an in-memory PDF with the fields
+    io_obj = io.BytesIO()
+    _create_only_fields(io_obj, fields_per_page)
+    temp_pdf = Pdf.open(io_obj)
+
+    foreign_root = in_pdf.copy_foreign(temp_pdf.Root)
+    in_pdf.Root.AcroForm = foreign_root.AcroForm
+    for in_page, temp_page in zip(in_pdf.pages, temp_pdf.pages):
+        if not hasattr(temp_page, 'Annots'):
+            continue # no fields on this page, skip
+        annots = temp_pdf.make_indirect(temp_page.Annots)
+        if not hasattr(in_page, 'Annots'):
+            in_page['/Annots'] = in_pdf.copy_foreign(annots)
+        else:
+            in_page.Annots.extend(in_pdf.copy_foreign(annots))
+    in_pdf.save(out_file)
+
+
+####### OpenCV related functions #########
+
+def get_possible_fields(in_pdf_file):
+    dpi = 200
+    images = convert_from_path(in_pdf_file, dpi=dpi)
+
+    tmp_files = [tempfile.NamedTemporaryFile() for i in range(len(images))]
+    for file_obj, img in zip(tmp_files, images):
+        img.save(file_obj, 'JPEG')
+        file_obj.flush()
+    bboxes_per_page = [get_contours(tmp_file.name) for tmp_file in tmp_files]
+
+    pts_in_inch = 72
+    unit_convert = lambda pix: pix / dpi * pts_in_inch
+
+    def img2pdf_coords(img, max_height):
+        # If bbox: X, Y, width, height, and whatever else you want (we won't return it)
+        if len(img) >= 4:
+            return (unit_convert(img[0]), unit_convert(max_height - img[1]), unit_convert(img[2]), unit_convert(img[3]))
+        # If just X and Y
+        elif len(img) >= 2:
+            return (unit_convert(img[0]), unit_convert(max_height - img[1]))
+        else:
+            return (unit_convert(img[0]))
+
+    new_coords = [ [img2pdf_coords(bbox, images[i].height) for bbox in bboxes_in_page] 
+                  for i, bboxes_in_page in enumerate(bboxes_per_page)]
+    return new_coords
+
+def intersect_bbox(bbox_a, bbox_b, dialation=2) -> bool:
+    a_left, a_right = bbox_a[0] - dialation, bbox_a[0] + bbox_a[2] + dialation
+    a_bottom, a_top = bbox_a[1] - dialation, bbox_a[1] + bbox_a[3] + dialation
+    b_left, b_right = bbox_b[0], bbox_b[0] + bbox_b[2]
+    b_bottom, b_top = bbox_b[1], bbox_b[1] + bbox_b[3]
+    if a_bottom > b_top or a_top < b_bottom:
+        return False
+    if a_left > b_right or a_right < b_left:
+        return False
+    return True
+    
+def get_contours(in_file):
+    """
+    Caveats so far: only considers straight, normal horizonal lines that don't touch any vertical lines as fields
+    Won't find field inputs as boxes
+    """
+    # 0 is for the flags: means nothing special is being used
+    img = cv2.imread(in_file, 0)
+
+    # fixed level thresholding, turning a gray scale / multichannel img to a black and white one.
+    # OTSU = optimum global thresholding: minimizes the variance of each Thresh "class"
+    # for each possible thresh value between 128 and 255, split up pixels, get the within-class variance,
+    # and minimize that
+    (thresh, img_bin) = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY|cv2.THRESH_OTSU)
+
+    img_bin = 255 - img_bin
+    cv2.imwrite("Image_bin.png", img_bin)
+
+    # Detect horizontal lines and vertical lines
+    kernel_length = np.array(img).shape[1]//40
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    vert_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, kernel_length))
+    vertical_lines_img = cv2.dilate(cv2.erode(img_bin, vert_kernel, iterations=3), vert_kernel, iterations=3)
+    horiz_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_length, 1))
+    horizontal_lines_img = cv2.dilate(cv2.erode(img_bin, horiz_kernel, iterations=3), horiz_kernel, iterations=3)
+
+    alpha = 0.5
+    img_final_bin = cv2.addWeighted(vertical_lines_img, alpha, horizontal_lines_img, 1.0 - alpha, 0.0)
+
+    contours, _ = cv2.findContours(img_final_bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    def sort_contours(cnts, method="left-to-right"):
+        reverse = False
+        coord = 0
+        if method == "right-to-left" or method == "bottom-to-top":
+            reverse = True
+        # handle sorting against the y-coord rather than the x-coord of the bounding box
+        if method == "top-to-bottom" or method == "bottom-to-top":
+            coord = 1
+        # construct list of bounding boxes and sort them top to bottom
+        boundingBoxes = [cv2.boundingRect(c) for c in cnts]
+        (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
+            key=lambda b:b[1][coord], reverse=reverse))
+        # return the list of sorted contours and bounding boxes
+        return (cnts, boundingBoxes)
+    (contours, boundingBoxes) = sort_contours(contours, method='top-to-bottom')
+    vert_contours, _ = cv2.findContours(vertical_lines_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    if vert_contours:
+        # Don't consider horizontal lines that meet up against vertical lines as text fields
+        (vert_contours, vert_bounding_boxes) = sort_contours(vert_contours, method='top-to-bottom')
+        to_return = []
+        for bbox in boundingBoxes:
+            inters = [intersect_bbox(vbbox, bbox) for vbbox in vert_bounding_boxes]
+            if not any(inters):
+                to_return.append(bbox)
+        return to_return
+    else:
+        return boundingBoxes
+
+def auto_add_fields(in_pdf_file, out_pdf_file):
+    bboxes_per_page = get_possible_fields(in_pdf_file)
+    fields = [ [FormField(f'page_{i}_field_{j}', 'text', bbox[0], bbox[1], configs={'width': bbox[2], 'height': 20})
+                for j, bbox in enumerate(bboxes_in_page)]
+                for i, bboxes_in_page in enumerate(bboxes_per_page)]
+    print(fields)
+    set_fields(in_pdf_file, out_pdf_file, fields)
