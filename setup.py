c7935b8c2106329696a6653836fed0ad2f29400e
David Colarusso <colarusso@users.noreply.github.com>
2022-02-14 11:06:46 -0500

    next build no

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='formfyxer',
-    version='0.0.4',
+    version='0.0.5',
     author='Suffolk LIT Lab',
     author_email='litlab@suffolk.edu',
     description='A tool for learning about and pre-processing pdf forms.',
