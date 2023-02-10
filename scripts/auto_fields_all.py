71d903804b0178ff409dd15c49785663fcaf59c6
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2023-02-10 09:27:32 -0500

    Refactor for relabeling fields (#90)
    ## Rename swap_pdf_page to copy_pdf_fields. 
    
    Fix #32
    
    ##  Large refactor to enable re-labeling fields
    
    * Use FormField objects when possible, instead of passing around
      raw Pike pdf objects
      * be gets font size from DA (PDF spec 12.7.3.2)
    * group fields by page (needed to match fields to surrounding text)
    * filter out fields without a page, or with a Template page (PDF spec 12.7.6)
    * make names longer when reading them (PDF spec 12.7.3.2)
    * improve auto_fields_all.py, with proper arguments and everything
      * lets you take all PDF in a folder and either
        * add fields if there aren't any
        * add fields if there aren't any and relabel existing fields
        * scrap existing fields and add fields back with our recognition
        * do nothing: just copy the PDFs

--- scripts/auto_fields_all.py
+++ scripts/auto_fields_all.py
@@ -1,20 +1,58 @@
 #!/usr/bin/env python3
 
-import sys
 import os
 import tempfile
 from pikepdf import Pdf
+import argparse
+import shutil
 
-from formfyxer.pdf_wrangling import auto_add_fields
+from pathlib import Path
+from enum import Enum
+import traceback
 
 
-def main():
+class ProcessingOption(Enum):
+    AUTO_ADD = "autoadd"
+    AUTO_AND_RELABEL = "autoandrelabel"
+    SCRAP_AND_AUTO = "scrapandauto"
+    NOTHING = "nothing"
+
+    def __str__(self):
+        return self.value
+
+
+def main() -> None:
     """Pass in an in-folder with PDFs, we'll strip off the fields and run our stuff over them"""
-    if len(sys.argv) < 3:
-        print("Need to pass in an in folder and a out folder!")
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-f",
+        "--fieldsredo",
+        type=str,
+        help="""
+      Determine what to do with fields. If "autoadd" (default), automatically adds fields when there
+      aren't any in the PDF. If "autoandrelabel", automatically adds fields, and renames
+      existing PDF fields using our ML. If "scrapandauto", it erases existing PDF fields and does auto add.
+      If "nothing", doesn't do anything, and will just copy all of the PDFs into another folder.
+      """,
+        default="autoadd",
+    )
+    parser.add_argument(
+        "in_folder",
+        type=str,
+        help="The input folder. All PDFs from this folder will be processed",
+    )
+    parser.add_argument("out_folder", type=str, help="The output folder")
+    args = parser.parse_args()
+    in_folder = args.in_folder
+    out_folder = args.out_folder
+    try:
+        p_option = ProcessingOption(
+            args.fieldsredo.lower().replace("-", "").replace("_", "")
+        )
+    except KeyError:
+        print(f"--fieldsredo needs to be a valid value (you passed {args.fieldsredo})")
         return
-    in_folder = sys.argv[1]
-    out_folder = sys.argv[2]
+
     to_process = sorted(
         [
             in_file
@@ -22,19 +60,51 @@ def main():
             if in_file.lower().endswith(".pdf")
         ]
     )
+
+    out_path = Path(out_folder)
+    if not Path(out_folder).exists():
+        out_path.mkdir(parents=True)
     for in_file in to_process:
+        in_path = in_folder + "/" + in_file
+        print(f"Starting on {in_path}")
         try:
-            p = Pdf.open(in_folder + "/" + in_file)
-            print(f"Starting on {in_file}")
-            p.Root.AcroForm = []
-            for page in p.pages:
-                page.Annots = []
-            tmp_file = tempfile.NamedTemporaryFile()
-            p.save(tmp_file.name)
-            tmp_file.flush()
-            auto_add_fields(tmp_file.name, out_folder + "/" + in_file)
+            if p_option in [
+                ProcessingOption.AUTO_ADD,
+                ProcessingOption.AUTO_AND_RELABEL,
+            ]:
+                from formfyxer.pdf_wrangling import (
+                    auto_add_fields,
+                    get_existing_pdf_fields,
+                    auto_rename_fields,
+                )
+
+                all_fields = [
+                    f
+                    for f_in_page in get_existing_pdf_fields(in_path)
+                    for f in f_in_page
+                ]
+                if not all_fields:
+                    auto_add_fields(in_path, out_folder + "/" + in_file)
+                else:
+                    if p_option == ProcessingOption.AUTO_AND_RELABEL:
+                        auto_rename_fields(in_path, out_folder + "/" + in_file)
+                    else:
+                        shutil.copyfile(in_path, out_folder + "/" + in_file)
+            elif p_option == ProcessingOption.SCRAP_AND_AUTO:
+                from formfyxer.pdf_wrangling import get_existing_pdf_fields
+
+                p = Pdf.open(in_path)
+                p.Root.AcroForm = []
+                for page in p.pages:
+                    page.Annots = []
+                tmp_file = tempfile.NamedTemporaryFile()
+                p.save(tmp_file.name)
+                tmp_file.flush()
+                auto_add_fields(tmp_file.name, out_folder + "/" + in_file)
+            else:
+                shutil.copyfile(in_path, out_folder + "/" + in_file)
         except Exception as ex:
-            print(f"Got exception for {in_file}: {ex}")
+            print(f"Got exception for {in_file}: {ex}, {traceback.format_exc()}")
 
 
 if __name__ == "__main__":
