897e0ccc2d02ab6311348618fe64d72aef0c3e9b
David Colarusso <colarusso@users.noreply.github.com>
2024-10-23 11:56:35 -0400

    quick and dirty patch
    I removed all uses of SpaCy, hopefully, stepping around the conflict that presented in the Weaver with updates to other packages. I was only able to do this, however, by making use of the tools.suffolklitlab.org micro-services: `vectorize` and `passive`. You can see the testing results in `dev-testing/test package.ipynb` Unfortunately, this means that there are now a number of features that require a tools_token to work. I have not updated the documentation to make this clear, but I will open an issue. Also, I'm not sure if we want to push this change to PiPy or have the Weaver install from this branch as a hot fix. Obviously, where Weaver calls these functions no using tools, we'll need to pass a token.

--- setup.py
+++ setup.py
@@ -6,17 +6,17 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 # We can't simply include this as an install_requires, because pypi won't allow
 # projects with github dependencies to be hosted there.
-class InstallSpacyModelCommand(install):
-    def run(self):
-        install.run(self)
-        import spacy
-        print("Downloading word2vec model en_core_web_sm")
-        spacy.cli.download('en_core_web_sm')
+#class InstallSpacyModelCommand(install):
+#    def run(self):
+#        install.run(self)
+#        import spacy
+#        print("Downloading word2vec model en_core_web_sm")
+#        spacy.cli.download('en_core_web_sm')
 
 
 setuptools.setup(
     name='formfyxer',
-    version='0.3.0a2',
+    version='0.3.0a3',
     author='Suffolk LIT Lab',
     author_email='litlab@suffolk.edu',
     description='A tool for learning about and pre-processing pdf forms.',
@@ -35,8 +35,8 @@ setuptools.setup(
         'typer>=0.4.1,<0.5.0', # typer pre 0.4.1 was broken by click 8.1.0: https://github.com/explosion/spaCy/issues/10564
         'openai', 'python-docx', 'tiktoken', 'transformers' 
     ],
-    cmdclass={
-      'install': InstallSpacyModelCommand,
-    },
+    #cmdclass={
+    #  'install': InstallSpacyModelCommand,
+    #},
     include_package_data = True
 )
